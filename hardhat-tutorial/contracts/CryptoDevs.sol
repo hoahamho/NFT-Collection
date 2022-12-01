710239b3d0a3fb9e4a5724289b1bf33d3801e20b
hoahamho <hoaciven@gmail.com>
2022-12-01 10:39:20 +0700

    b

--- hardhat-tutorial/contracts/CryptoDevs.sol
+++ hardhat-tutorial/contracts/CryptoDevs.sol
@@ -13,14 +13,14 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
      */
     string _baseTokenURI;
 
-    // _price is the price of one Crypto Dev NFT
+    //  _price is the price of one Crypto Dev NFT
     uint256 public _price = 0.01 ether;
 
-    // _pause is used to pause the contract in case of emergency
+    // _paused is used to pause the contract in case of an emergency
     bool public _paused;
 
-    // max number of Cryptodevs
-    uint256 public maxTokenId = 20;
+    // max number of CryptoDevs
+    uint256 public maxTokenIds = 20;
 
     // total number of tokenIds minted
     uint256 public tokenIds;
@@ -35,7 +35,7 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
     uint256 public presaleEnded;
 
     modifier onlyWhenNotPaused() {
-        require(!_paused, "Contract currently paused!");
+        require(!_paused, "Contract currently paused");
         _;
     }
 
@@ -58,8 +58,8 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
      */
     function startPresale() public onlyOwner {
         presaleStarted = true;
-        // set presaleEnded time as current timestamp + 5 minutes
-        // solidity has cool syntax for timestamp (seconds, minutes, hours, days, years)
+        // Set presaleEnded time as current timestamp + 5 minutes
+        // Solidity has cool syntax for timestamps (seconds, minutes, hours, days, years)
         presaleEnded = block.timestamp + 5 minutes;
     }
 
@@ -75,12 +75,26 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
             whitelist.whitelistedAddresses(msg.sender),
             "You are not whitelisted"
         );
-        require(tokenIds < maxTokenId, "Exceeded maximum Crypto Devs supply");
-        require(msg.value > _price, "Ether sent is not correct");
+        require(tokenIds < maxTokenIds, "Exceeded maximum Crypto Devs supply");
+        require(msg.value >= _price, "Ether sent is not correct");
         tokenIds += 1;
         //_safeMint is a safer version of the _mint function as it ensures that
         // if the address being minted to is a contract, then it knows how to deal with ERC721 tokens
         // If the address being minted to is not a contract, it works the same way as _mint
+        _safeMint(msg.sender, tokenIds);
+    }
+
+    /**
+     * @dev mint allows a user to mint 1 NFT per transaction after the presale has ended.
+     */
+    function mint() public payable onlyWhenNotPaused {
+        require(
+            presaleStarted && block.timestamp >= presaleEnded,
+            "Presale has not ended yet"
+        );
+        require(tokenIds < maxTokenIds, "Exceed maximum Crypto Devs supply");
+        require(msg.value >= _price, "Ether sent is not correct");
+        tokenIds += 1;
         _safeMint(msg.sender, tokenIds);
     }
 
@@ -107,7 +121,7 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
         address _owner = owner();
         uint256 amount = address(this).balance;
         (bool sent, ) = _owner.call{value: amount}("");
-        require(sent, "Failed to sent Ether");
+        require(sent, "Failed to send Ether");
     }
 
     // Function to receive Ether. msg.data must be empty
